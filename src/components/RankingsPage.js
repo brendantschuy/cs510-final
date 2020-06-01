65c5e1ce5e9acbdefb78a99c776372a7c264d61c
Brendan <brendantschuy@gmail.com>
2020-06-01 00:47:22 -0700

    Completed rankings API fetch.

--- src/components/RankingsPage.js
+++ src/components/RankingsPage.js
@@ -1,24 +1,42 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
+/* rank, school [name], conference, firstPlaceVotes, points */
+
 const rankingsUrl = "https://api.collegefootballdata.com/rankings?year=2019&seasonType=regular";
 
 const RankingsPage = () => {
 
-    const [data, setData] = useState();
+    const [data, setData] = useState([]);
+    let [annualData, setAnnualData] = useState([]);
 
     const fetchData = useCallback(async () => {
-        let response = await fetch(rankingsUrl);
-        response = await response.json();
-        setData(response);
+        fetch(rankingsUrl)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setData(res);
+                selectWeek(res);
+            });
     }, []);
+
+    const selectYear = (res, year = 2020) => 
+        `https://api.collegefootballdata.com/rankings?year=${year}&seasonType=regular`;
+
+    const selectWeek = (res, week = 15) => {
+        setAnnualData(res[week].polls[1].ranks);
+    }
 
     useEffect(() => {
         fetchData();
-    }, [fetchData]);
+    }, []);
 
     return (
         <div>
-            <div>data: {JSON.stringify(data)}</div>
+            <div>
+                <ol>
+                {annualData.map(item => <li key={item.id}>{item.school}</li>)}
+                </ol>
+            </div>
         </div>
     )
 }
