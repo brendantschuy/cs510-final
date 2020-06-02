a25263d42eb3e13e29d0ac9329cfa4826c800f15
Brendan <brendantschuy@gmail.com>
2020-06-02 16:41:13 -0700

    Started implement conferences page.

---
+++ src/components/Conference.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Conference = data => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+export default Conference;
--- src/components/ConferencesPage.js
+++ src/components/ConferencesPage.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import '../styles/teams.css';
 
 const ConferencesPage = () => {
+
+    const [ data, setData ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    const conferences = [
+        "American Athletic",
+        "ACC",
+        "Big 12",
+        "Big Ten",
+        "Conference USA",
+        "FBS Independents",
+        "Mid-American",
+        "Mountain West",
+        "Pac-12",
+        "SEC",
+        "Sun Belt"
+    ];
+
+    const year = 2019;
+
+    const fetchData = useCallback(async () => {
+        const url = `https://api.collegefootballdata.com/teams/fbs?year=${year}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                setData(res);
+                setLoading(false);
+                console.log(res);
+            });
+    }, []); //use empty array to avoid infinite loop
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <div>
+        <div className="conferences-container">
+
             Conference Page
         </div>
     );
