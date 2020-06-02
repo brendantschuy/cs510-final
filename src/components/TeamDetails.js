347dd13457026a45d22da6b529fe5aa6b4e3545a
Brendan <brendantschuy@gmail.com>
2020-06-02 12:00:18 -0700

    Added images to team details page

--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
-const TeamDetails = ({ match, location }) => {
+const TeamDetails = ({ match, }) => {
 
     const { params: { teamName } } = match;
     
@@ -9,49 +9,42 @@ const TeamDetails = ({ match, location }) => {
 
     const year = 2019;
 
-    const placeHolder = {
-        abbreviation: "AFA",
-        alt_color: "#ffffff",
-        alt_name1: null,
-        alt_name2: "AFA",
-        alt_name3: "Air Force",
-        color: "#004a7b",
-        conference: "Mountain West",
-        division: "Mountain",
-        highlight: true,
-        id: 2005,
-        logos: ["http://a.espncdn.com/i/teamlogos/ncaa/500/2005.png", "http://a.espncdn.com/i/teamlogos/ncaa/500-dark/2005.png"],
-        mascot: "Falcons",
-        school: "Air Force",
-    };
-
-    const { abbreviation, alt_color, alt_name2, color, conference, division, logos, mascot, school } = placeHolder;
-    const logo1 = logos[0];
-    const logo2 = logos[1];
-
     const fetchData = useCallback(async () => {
         const url1 = `https://api.collegefootballdata.com/records?year=${year}&team=${teamName.replace(" ","%20")}`;
         const url2 = `https://api.collegefootballdata.com/teams/fbs?year=${year}`;
+
         fetch(url1)
             .then(res => res.json())
             .then(res => {
-                console.log(res);
-                setData(res);
-                setDisplay(
-                    [res[0].total.wins,
+                setData(data => ([
+                    res[0].total.wins, 
+                    res[0].total.losses, 
+                    res[0].team, 
+                    res[0].conference,
+                    ...data
+                ]));
+
+                setDisplay(display => ([
+                    res[0].total.wins,
                     res[0].total.losses,
                     res[0].team,
-                    res[0].conference]
-                );
+                    res[0].conference,
+                    ...display
+                ]));
+                
             });
         fetch(url2)
             .then(res => res.json())
             .then(res => {
-                console.log(res);
-                setData(...data, res);
-                //setDisplay(...display, )
+                const thisSchool = res.find(({school}) =>
+                    school == teamName
+                );
+                const { mascot } = thisSchool;
+                const logoUrl = thisSchool.logos[0];
+                setData(data => ([...data, mascot, logoUrl]));
+                setDisplay(display => ([...display, mascot, logoUrl]));
             })
-    }, []); //use empty array to avoid infinite loop
+    }, [ ]); //use empty array to avoid infinite loop
 
     useEffect(() => {
         fetchData();
@@ -59,10 +52,10 @@ const TeamDetails = ({ match, location }) => {
 
     return (
         <div className="details-container">
-            {/* <img className="details-logo" src={logo1}></img> */}
+            <img className="details-logo" src={display[5]}></img>
             <p class="details-title">
                 <span className="details-heading">{display[2]}</span>
-                {/* <span className="details-sub-heading">{mascot}</span> */}
+                <span className="details-sub-heading">{display[4]}</span>
             </p>
             <p class="details-title">
                 <span className="details-sub-heading">{display[3]}</span>
