9fefcb5c43a8a88caabe5cc87c8abfb3a156c330
Brendan <brendantschuy@gmail.com>
2020-06-02 17:24:37 -0700

    Add search to Schedule page. Add return to top button.

--- src/components/GameEvent.js
+++ src/components/GameEvent.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 
-const GameEvent = ({data}) => {
+const GameEvent = forwardRef((props, ref) => {
 
-    const { home_team, away_team, start_date, venue } = data;
+    const { home_team, away_team, start_date, venue } = props.data;
 
     const parseDate = dateTime => {
         const date = dateTime.split("T")[0];
@@ -11,15 +11,18 @@ const GameEvent = ({data}) => {
     }
 
     return (
-        <div className="schedule-game">
-            <span className="schedule-game-teams">
-                {home_team} @ {away_team}
-                <br/>
-                {parseDate(start_date)} at {venue}
+        <div className={`schedule-game${ref ? " highlight" : ""}`} ref={ref}>
+            <span className={`schedule-game-teams`}>
+                <span style={{float: "left"}}>
+                    <a href={`/teams/${home_team.replace(" ", "%20")}`} className="schedule-game-link">{home_team} </a>
+                    @
+                    <a href={`/teams/${away_team.replace(" ", "%20")}`} className="schedule-game-link"> {away_team}</a>
+                    </span>
+                <span style={{float: "right"}}>{parseDate(start_date)} at {venue}</span>
             </span>
         </div>
     );
-};
+});
 
 
 export default GameEvent;
--- src/components/SchedulePage.js
+++ src/components/SchedulePage.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import GameEvent from './GameEvent';
 import Spinner from 'react-bootstrap/Spinner'
 
@@ -8,6 +8,10 @@ const SchedulePage = () => {
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [isSearched,] = useState(false);
+    const [searchState, setSearchState] = useState("");
+
+    const searchRef = useRef(null);
 
     const fetchData = useCallback(async () => {
         const url = `https://api.collegefootballdata.com/games?year=2020&week=1&seasonType=regular`;
@@ -25,15 +29,40 @@ const SchedulePage = () => {
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [isSearched]);
 
-    console.log("data  ", data);
+    const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop);
+    const executeScroll = () => scrollToRef(searchRef);
+    const updateSearchState = ({target: {value: query}}) => setSearchState(query.toLowerCase());
+    
 
     return (
         <div className="schedule-container">
             Upcoming games:
-            {loading && <Spinner animation="border" role="status"/>}
-            {data.map(game => <GameEvent data={game} />)}
+            <div className="schedule-search">
+                <input
+                    type="text"
+                    className="schedule-search-input"
+                    placeholder="search for a team"
+                    style={{textAlign: "center"}}
+                    onChange={updateSearchState}
+                ></input>
+            </div>
+            <button class="schedule-search-button" onClick={executeScroll}>Find</button>
+            {loading && <Spinner animation="border" role="status" />}
+            {data.map(game =>
+                <GameEvent
+                    data={game}
+                    ref={(game.home_team.toLowerCase() === searchState || game.away_team.toLowerCase() === searchState)
+                         ? searchRef 
+                         : null}
+                />
+            )}
+            <div className="return-to-top">
+                <button onClick={() => window.scrollTo(0, 0)}>
+                    Return to top
+                </button>
+            </div>
         </div>
     );
 };
