5e0c98fd51dde7bcc35d46a789ac1cd1ad4bf345
Brendan <brendantschuy@gmail.com>
2020-06-02 21:43:22 -0700

    Updates

---
+++ precache-manifest.74eb4293a6b1757df1f77080f547eb1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c712f0871746391989ee4ea58e5e9774",
+    "url": "/brendantschuy/cs510-finalindex.html"
+  },
+  {
+    "revision": "f9723dff07a43a3c138c",
+    "url": "/brendantschuy/cs510-finalstatic/css/main.eab7a262.chunk.css"
+  },
+  {
+    "revision": "c2099abec85ff6c18203",
+    "url": "/brendantschuy/cs510-finalstatic/js/2.ac9651da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/brendantschuy/cs510-finalstatic/js/2.ac9651da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9723dff07a43a3c138c",
+    "url": "/brendantschuy/cs510-finalstatic/js/main.ec06c823.chunk.js"
+  },
+  {
+    "revision": "5fcc16a6372e18757933",
+    "url": "/brendantschuy/cs510-finalstatic/js/runtime-main.96497e55.js"
+  },
+  {
+    "revision": "cc65024dc4a05127451f3ea4e026a820",
+    "url": "/brendantschuy/cs510-finalstatic/media/football.cc65024d.png"
+  }
+]);
